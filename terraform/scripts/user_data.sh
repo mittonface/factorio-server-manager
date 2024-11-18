a7694286c7a5ac7470cd0f304df667110a6cf461
Brent Mitton <brent.mitton@gmail.com>
2024-11-17 20:35:22 -0500

    make mount permanent, create factorio service

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -2,22 +2,25 @@
 # Log all output
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-
 # Update system
 apt update
 apt upgrade -y
 
 # Install required packages for EFS
-apt install -y nfs-common jq awscli
+apt install -y nfs-common jq awscli git binutils rustc cargo pkg-config libssl-dev
+
+# Manually installed EFS Utils
+git clone https://github.com/aws/efs-utils
+cd efs-utils
+./build-deb.sh
+apt install -y ./build/amazon-efs-utils*deb
 
 # Create mount directory
 mkdir -p /mnt/efs
 
-# Mount EFS
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_dns_name}:/ /mnt/efs
-
-# Make mount permanent
-echo "${efs_dns_name}:/ /mnt/efs nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0" >> /etc/fstab
+# Mount EFS using efs-utils
+echo "${efs_dns_name}:/ /mnt/efs efs _netdev,tls,iam 0 0" >> /etc/fstab
+mount -a
 
 # Fetch secrets and set environment variables
 SECRET_JSON=$(aws secretsmanager get-secret-value \
@@ -48,21 +51,64 @@ chown -R ubuntu:ubuntu /mnt/efs
 cd /mnt/efs
 wget -O linux64.tar.xz https://factorio.com/get-download/stable/headless/linux64
 tar -xf linux64.tar.xz
-
 wget https://raw.githubusercontent.com/mittonface/factorio-server-manager/refs/heads/main/terraform/scripts/server-settings.json
 wget https://raw.githubusercontent.com/mittonface/factorio-server-manager/refs/heads/main/terraform/scripts/map-gen-settings.json
 wget https://raw.githubusercontent.com/mittonface/factorio-server-manager/refs/heads/main/terraform/scripts/map-settings.json
-
-echo "Setup completed at $(date)" > /tmp/setup-completed.txt
 
 # Replace the variables in server-settings.json with actual values
 sed -i "s/\$FACTORIO_USERNAME/$FACTORIO_USERNAME/g" server-settings.json
 sed -i "s/\$FACTORIO_PASSWORD/$FACTORIO_PASSWORD/g" server-settings.json
 sed -i "s/\$GAME_PASSWORD/$GAME_PASSWORD/g" server-settings.json
 
-# this _should_ only be run the first time the instance is created. 
-# feels like maybe still the wrong time to run it
-./factorio/bin/x64/factorio --create saves/boy-save.zip --map-gen-settings /mnt/efs/map-gen-settings.json --map-settings /mnt/efs/map-settings.json
+chown -R ubuntu:ubuntu /mnt/efs
+find /mnt/efs -type d -exec chmod 755 {} \;
+find /mnt/efs -type f -exec chmod 644 {} \;
+chmod +x /mnt/efs/factorio/bin/x64/factorio
 
-# reboot so the profile.d script we just created will be run
-reboot
+cd ./factorio
+# Create initial save if it doesn't exist
+if [ ! -f saves/boy-save.zip ]; then
+    ./bin/x64/factorio --create saves/boy-save.zip --map-gen-settings /mnt/efs/map-gen-settings.json --map-settings /mnt/efs/map-settings.json
+fi
+
+
+# Create systemd service for Factorio
+cat << 'EOF' > /etc/systemd/system/factorio.service
+[Unit]
+Description=Factorio Server
+After=network-online.target remote-fs.target
+Wants=network-online.target
+RequiresMountsFor=/mnt/efs
+
+[Service]
+Type=simple
+User=ubuntu
+Group=ubuntu
+WorkingDirectory=/mnt/efs
+ExecStart=/mnt/efs/factorio/bin/x64/factorio --start-server-load-latest --server-settings /mnt/efs/server-settings.json
+Restart=on-failure
+RestartSec=10
+StandardOutput=journal
+StandardError=journal
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# make sure hte permissions are correct before trying to start the service
+chown -R ubuntu:ubuntu /mnt/efs
+
+# Enable and start Factorio service
+systemctl daemon-reload
+systemctl enable factorio.service
+systemctl start factorio.service
+
+# Write a cloud-init success file
+cat << EOF > /var/lib/cloud/instance/success.txt
+success: $(date)
+EOF
+
+# Instead of rebooting, just source the environment file
+source /etc/profile.d/factorio-env.sh
+
+echo "Setup completed at $(date)" > /tmp/setup-completed.txt
