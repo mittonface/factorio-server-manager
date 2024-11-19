8beb83855a249f0021b58b164b53f2c803243737
Brent Mitton <brent.mitton@gmail.com>
2024-11-18 21:13:47 -0500

    don't get fancy with cross platform builds

--- build-push.sh
+++ build-push.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-
 # Get AWS account ID
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 AWS_REGION="us-east-1"  # Change to your region
 ECR_REPO_NAME="bm-factorio-image"
-
 # Get the current git commit hash
 GIT_COMMIT=$(git rev-parse --short HEAD)
-
-# Install QEMU emulators
-docker run --privileged --rm tonistiigi/binfmt --install all
-
-# Create and use a new builder that supports multi-architecture builds
-docker buildx create --name multiarch-builder --use
 
 # Login to ECR
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
-# Build and push the image directly with buildx
-docker buildx build \
-  --platform linux/amd64 \
-  --push \
+# Build the image normally
+docker build --platform=linux/amd64 \
   -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:${GIT_COMMIT} \
   -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:latest \
   .
+
+# Push both tags
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:${GIT_COMMIT}
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:latest
